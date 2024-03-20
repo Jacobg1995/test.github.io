4c4ef84f9dcdcd852ac193ea31e67ad45b434ff3
Jake Green <jacob.green@byte-engineer.co.uk>
2024-03-20 11:02:10 +0000

    Test

--- js/bot_api_converters/osbot/osbot_path_converter.js
+++ js/bot_api_converters/osbot/osbot_path_converter.js
@@ -29,7 +29,7 @@ export class OSBotPathConverter extends OSBotConverter {
     toRaw(path) {
         var output = "";
         for (var i = 0; i < path.positions.length; i++) {
-            let regionId = Region.fromPositionpath.positions[i].id;
+            let regionId = Region.fromPosition(path.positions[i]).id;
             let x = (path.positions[i].x & 63);
             let y = (path.positions[i].y & 63);
             console.info("New Region Position: ", x);
